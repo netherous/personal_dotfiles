cf8e0103674bafb81709863219f203e017627543
George J Yi <39496197+netherous@users.noreply.github.com>
2023-07-04 03:57:02 -0500

    Update Nvimtree after.lua

--- nvim/lua/after.lua
+++ nvim/lua/after.lua
@@ -1,3 +1,21 @@
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
+
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
   ensure_installed = {"help","c", "lua", "rust" },
